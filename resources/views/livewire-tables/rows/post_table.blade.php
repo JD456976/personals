3fce168bc6ab881aa96012ec294ae3e1fe2da5c1
Jason <craig219@comcast.net>
2022-02-04 20:15:00 -0800

    Featured Post Styling

--- resources/views/livewire-tables/rows/post_table.blade.php
+++ resources/views/livewire-tables/rows/post_table.blade.php
@@ -8,7 +8,7 @@
               <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-star me-25"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon></svg>
               <span>FEATURED</span>
               <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-star me-25"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon></svg>
-            </span>
+        </span>
     @endif
 </x-livewire-tables::bs5.table.cell>
 
