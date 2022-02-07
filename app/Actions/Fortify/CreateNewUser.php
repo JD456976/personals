0bc76381ea3602b1bc9b7501541f362bcb0d19f2
Jason <craig219@comcast.net>
2022-02-06 22:35:19 -0800

    ip

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -36,7 +36,7 @@ class CreateNewUser implements CreatesNewUsers
         return User::create([
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
-            'ip' => Request::ip(),
+            'ip' => $input['ip'],
         ]);
     }
 }
