4eb761acdacf1ea882272d496f48aed322f45582
Jason <craig219@comcast.net>
2022-02-08 16:31:56 -0800

    Update 404.blade.php

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -77,7 +77,7 @@
                 <div class="misc-inner p-2 p-sm-3">
                     <div class="w-100 text-center">
                         <h2 class="mb-1">Page Not Found 🕵🏻‍♀️</h2>
-                        <p class="mb-2">Oops! 😖 The requested URL was not found on this server.</p><a class="btn btn-primary mb-2 btn-sm-block" href="{{route('home')}}">Back to home</a><img class="img-fluid" src="../../../app-assets/images/pages/error.svg" alt="Error page" />
+                        <p class="mb-2">Oops! 😖 The requested URL was not found on this server.</p><a class="btn btn-primary mb-2 btn-sm-block" href="{{route('home')}}">Back to home</a><img class="img-fluid" src="" alt="Error page" />
                     </div>
                 </div>
             </div>
