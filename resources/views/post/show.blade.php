90022645ea419b549a6aa68b37f574f9e806658b
Jason <craig219@comcast.net>
2022-02-09 12:00:10 -0800

    Update show.blade.php

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -81,10 +81,12 @@
                                 </div>
                                 {{ views($post)->count() }}
                             </div>
+                            @if(!Auth::user()->id == $post->user_id)
                             <div>
                                 <button data-bs-toggle="modal" data-bs-target="#large" type="button" class="btn btn-primary waves-effect waves-float waves-light">REPLY</button>
                                 @include('partials.reply-modal')
                             </div>
+                            @endif
                         </div>
                     </div>
                 </div>
